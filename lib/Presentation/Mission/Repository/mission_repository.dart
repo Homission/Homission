c9f515e7e6683f375af38a4819e662bf88b44aab
KoJeongmin <terrantrace@gmail.com>
2024-06-23 15:14:41 +0900

    [KAN-52] feat: 미션 상세 화면 관련 데이터 요청 로직 추가 (참여자 조회)

--- lib/Presentation/Mission/Repository/mission_repository.dart
+++ lib/Presentation/Mission/Repository/mission_repository.dart
@@ -2,8 +2,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:homission/Presentation/Mission/Model/Mission.dart';
 import 'package:homission/Presentation/Mission/Model/UserMission.dart';
 
-
 abstract class MissionRepository {
   Future<List<Mission>> fetchMissions();
   Future<UserMission> fetchUserMission(String userId);
+  Future<double> fetchUserParticipationRate(String userId, int missionId);
 }
